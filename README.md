27e74d24b3a9b1e207a076297cec5bd54d242e31
8051Enthusiast <8051enthusiast@protonmail.com>
2023-04-27 02:42:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+allyourbase
+===========
+
+`allyourbase.py` is a script that tries to find the base address of a firmware image by comparing the addresses of strings with the target addresses of all possible pointers, similar to [rbasefind](https://github.com/sgayou/rbasefind).
+
+It works with arbritrary pointer sizes and endianness efficiently on decently sized firmware images (in the 10s of MBs, mostly limited by required RAM which grows linearly with filesize but has a high constant factor).
+
+Example
+-------
+```
+$ ./allyourbase.py -n 5 -l 8 -e little /usr/bin/ls
+Found 630 strings
+4/4
+Offset: 0x11ffffefc0
+```
+
+Parameters
+----------
+
+```
+-n N             the minimum length of strings to look for, in unicode codepoints
+-l L             the length of pointers to look for, in bytes (4 = 32-bit pointers
+-e {little,big}  the endianness of the pointers
+-a A             the alignment of the pointers (defaults to pointer lengths)
+-f F             slack factor (higher = slower and more memory but more accurate)
+```
+
+How it works
+------------
+First, one finds all addresses where strings are at and puts them in a set.
+Then one interpretes each (aligned) offset in the file as a pointer and puts the target addresses in a set.
+
+Now the brute-force approach would be to try each base address and look at how many string offsets overlap the possible pointer target locations.
+That way, one can simply choose the base address that has the highest overlap.
+
+However that approach is not really usable on 64-bit address spaces without changes, and it quickly gets slower with more string addresses.
+
+One useful realization is that "counting the overlap between to sets for each possible relative offset" is the same as doing the cross-correlation of the indicator vectors of the sets.
+This can be efficiently implemented using the fast fourier transform by the convolution theorem.
+Still you would have to do a fourier transform the size of the address space which in most cases is infeasable.
+
+Instead, if the target addresses are reduced modulo n (bigger than filesize) one can do a circular cross-correlation of size n to find out the base address modulo n.
+Doing that for a few coprime n until their product is bigger than the address space, one only has one solution that fits inside the address space which will probably be the base address.
+
+Assuming the target addresses do not tend to differ by multiples of n, the matches of strings with errant pointers modulo n is modeled as binomial noise.
+The purpose of the `-f` flag is to make the ratio of pointers in relation to n smaller so that the noise floor is lowered.
+
+License
+-------
+This software is licensed under the MIT license.
---
+++ allyourbase.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import sys
+import re
+import argparse
+import math
+from typing import Literal
+import numpy as np
+
+
+def get_string_addresses(b: bytes, n: int) -> set[int]:
+    """
+    Finds all utf8 strings of length at least n that are NUL-terminated and returns
+    the set of offsets
+    """
+    # this regex also matches overlong encodings and surrogates
+    # this is fine because this is a heuristic anyway
+    string_regex = (rb'(?:[\n\r\t\f\x20-\x7E\n]'
+                    rb'|[\xC2-\xDF][\x80-\xBF]'
+                    rb'|[\xE0-\xEF][\x80-\xBF]{2}'
+                    rb'|[\xF0-\xF4][\x80-\xBF]{3})'
+                    + f'{{{n},}}'.encode()
+                    + rb'\x00')
+    return {m.start() for m in re.finditer(string_regex, b)}
+
+
+def get_pointed_addresses(b: bytes, endian: Literal['little', 'big'], length: int, align: int) -> set[int]:
+    """
+    Interprets each offset in b as a pointer to an address in memory
+    and returns the set of all addresses that are pointed to.
+    """
+    pointed_addresses = set()
+    for i in range(0, len(b) - length + 1, align):
+        offset_bytes = b[i:i+length]
+        offset = int.from_bytes(offset_bytes, byteorder=endian)
+        pointed_addresses.add(offset)
+    return pointed_addresses
+
+
+def find_coprime_numbers(n: int, m: int) -> list[int]:
+    """
+    Returns a list of integers greater than n, whose product is greater than m and
+    that are pairwise coprime.
+    """
+    # only use odd numbers because with unaligned pointers there
+    # will be a lot of pointers that differ from each other by a
+    # multiple of 256, which interacts badly with the cross-correlation
+    i = n + 1 + (n % 2)
+    coprimes = [i]
+    product = i
+    i += 2
+    while product <= m:
+        if math.gcd(i, product) == 1:
+            coprimes.append(i)
+            product *= i
+        i += 2
+    return coprimes
+
+
+def find_max_overlap(A: set[int], B: set[int], modulos: list[int]) -> int:
+    """
+    Finds the probable offset between two sets of integers A and B, where the offset
+    is calculated by finding out the overlap modulo each modulus in the list.
+    The final offset is calculated using the Chinese remainder theorem.
+    """
+    offsets = []
+    print(f'0/{len(modulos)}', file=sys.stderr, end='', flush=True)
+    for p in modulos:
+        a = np.zeros(p, dtype=np.float64)
+        b = np.zeros(p, dtype=np.float64)
+        for x in A:
+            a[x % p] += 1
+        for x in B:
+            # reverse B to turn cross-correlation into convolution
+            b[(p - x % p) % p] += 1
+        # circular convolution of size p (per convolution theorem)
+        corr = np.fft.ifft(np.fft.fft(a) * np.fft.fft(b)).real
+        offset = int(np.argmax(corr))
+        offsets.append(offset)
+        print(f'\r{len(offsets)}/{len(modulos)}',
+              file=sys.stderr, end='', flush=True)
+    print(file=sys.stderr)
+
+    # chinese remainder theorem
+    M = math.prod(modulos)
+    k = sum(offset * (M // p) * pow(M // p, -1, p)
+            for p, offset in zip(modulos, offsets)) % M
+    return int(k)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="the file to analyze")
+    parser.add_argument(
+        "-n", help="the minimum length of strings to look for, in unicode codepoints", type=int, default=5)
+    parser.add_argument(
+        "-l", help="the length of pointers to look for, in bytes (4 = 32-bit pointers", type=int, default=8)
+    parser.add_argument("-e", help="the endianness of the pointers",
+                        choices=["little", "big"], default="little")
+    parser.add_argument(
+        "-a", help="the alignment of the pointers (defaults to pointer lengths)", type=int)
+    parser.add_argument(
+        "-f", help="slack factor (higher = slower and more memory but more accurate)", type=float)
+    args = parser.parse_args()
+
+    if args.a is None:
+        args.a = args.l
+
+    if args.f is None:
+        # make sure the ratio (number of pointers pointers/modulo) is at most 1/16 per default
+        # to make the noise floor reasonably low
+        args.f = min(1.0, 16.0 / args.a)
+
+    with open(args.file, "rb") as f:
+        b = f.read()
+        strings = get_string_addresses(b, args.n)
+        print(f'Found {len(strings)} strings', file=sys.stderr)
+        pointers = get_pointed_addresses(b, args.e, args.l, args.a)
+        modulos = find_coprime_numbers(
+            int(len(b) * args.f), 2**(args.l*8) + len(b))
+        offset = find_max_overlap(pointers, strings, modulos)
+
+        negative_offset = math.prod(modulos) - offset
+        if negative_offset < len(b):
+            print(f"Offset: -{negative_offset:#x}")
+        elif offset < 2**(args.l*8):
+            print(f"Offset: {offset:#x}")
+        else:
+            print("Offset: not found")
